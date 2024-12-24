412fb7ddd0e4927933a83dee4d451965f80255eb
Do-yeong Choi <han030111@kakao.net>
2024-12-24 16:50:23 +0900

    1

---
+++ 1181.py
@@ -0,0 +1,16 @@
+n = int(input())
+l = []
+
+for i in range(n):
+    l.append(input())
+    #단어 넣기
+l=list(set(l))
+l.sort()	
+#옛날에 정렬 검색을 많이 해서 abc순 정렬을 알고 있었다
+l.sort(key = len)	
+#길이순 정렬을 알아보다 찾아냈다
+
+
+for i in l:
+    print(i)
+    #하나씩 출력
