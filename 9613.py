cad22ccaf561ffb0537cccf4bb52def4603a4251
Do-yeong Choi <han030111@kakao.net>
2025-01-23 19:31:30 +0900

    1/23

---
+++ 9613.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/5347
+import sys
+import math
+a=int(input())
+
+for i in range(a):
+    l=list(map(int, sys.stdin.readline().split()))
+    value=[]
+    for i in range(1,len(l)):
+        for k in range(i+1,len(l)):
+            value.append(math.gcd(l[i],l[k]))
+            print(sum(value))
+   
+#블랙잭의 겹치지 않게 모든 경우의 수를 찾는 경험이 도움된것 같다
+#지금 생각해보면 gcd 
