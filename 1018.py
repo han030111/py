cdb2c2efbcb739da1eaca331c9a7e6545dbc5349
Do-yeong Choi <han030111@kakao.net>
2025-02-13 19:29:38 +0900

    2/12

--- 1018.py
+++ 1018.py
@@ -12,14 +12,14 @@ for i in range(N-7):
         for k in range(i,i+8):
             for l in range(j,j + 8):
                 if (k + l) % 2 == 0:
-                    if board[k][l] != 'W':
+                    if board[k][l] == 'B':
                         w +=1
-                    if board[k][l] != 'B':
+                    if board[k][l] == 'W':
                         b+=1
                 else:
-                    if board[k][l] != 'B':
+                    if board[k][l] == 'W':
                         w +=1
-                    if board[k][l] != 'W':
+                    if board[k][l] == 'B':
                         b+=1
         list.append(w)
         list.append(b)
