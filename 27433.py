d80b02845cbbe27b305d3d412b2efd384ea6ee7f
han030111 <han030111@kakao.com>
2025-06-22 10:14:05 +0900

    6/22

--- 27433.py
+++ 27433.py
@@ -1,12 +1,6 @@
 # https://www.acmicpc.net/problem/27433
-a,b=0,1
-c=int(input())
-if c==0:
-    print(0)
-elif c==1:
-    print(1)
-else:
-
-    for i in range(c-1):
-        a,b=b,a+b
-    print(b)
+import sys
+cnt=1
+for i in range(1,int(sys.stdin.readline())+1):
+    cnt*=i
+print(cnt)
